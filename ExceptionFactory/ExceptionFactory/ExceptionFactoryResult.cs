5f7fc03410f96b60053ea1475787ced1166a8b60
Johnathon <Johnathon@Sullinger.us>
2014-11-03 21:49:02 -0700

    Initial Commit.

---
+++ ExceptionFactory/ExceptionFactory/ExceptionFactoryResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sullinger.ExceptionFactory
+{
+    /// <summary>
+    /// The results of an ExceptionFactory invocation
+    /// </summary>
+    public class ExceptionFactoryResult
+    {
+        /// <summary>
+        /// Allows calling back to the ExceptionFactory requestor
+        /// </summary>
+        /// <param name="callback">The callback.</param>
+        public void ElseDo(Action callback)
+        {
+            callback();
+        }
+    }
+}
